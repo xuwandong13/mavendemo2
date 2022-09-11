283293b38326ff1df64f3178dfb279c158d26132
xuwandong <1215792197@qq.com>
2022-09-11 17:09:38 +0800

    test

---
+++ src/main/java/com/nowcoder/mavendemo2/App.java
@@ -0,0 +1,13 @@
+package com.nowcoder.mavendemo2;
+
+/**
+ * @author ：xuwandong
+ * @date ：Created in 2022/9/7 22:30
+ * @description：
+ * @modified By：
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello ");
+    }
+}
